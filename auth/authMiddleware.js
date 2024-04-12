38e2bb6c3c5a2f0a956e8745eb013f3a7dfa5464
Biswarup Ghosh <ghoshbiswarup414@gmail.com>
2024-04-12 16:34:05 +0530

    ver 1.1

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -1,19 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
-    console.log(req.headers)
     const token = req.headers.authorization;
-    console.log(token);
 
     if (!token || !token.startsWith("Bearer ")) {
-        return res.status(401).json({ error: 'Unauthorized: No token provided' });
+        return res.status(401).json({ error: 'Unauthorized: No token provided', msg: 'Please login first' });
     }
 
     const newToken = token.split(" ")[1];
 
     jwt.verify(newToken, process.env.JWT_SECRET, (err, decodedToken) => {
         if (err) {
-            return res.status(403).json({ error: 'Forbidden: Invalid token' });
+            return res.status(403).json({ error: 'Forbidden: Invalid token', msg: 'Please login first' });
         }
         req.user = decodedToken.user;
         next();
